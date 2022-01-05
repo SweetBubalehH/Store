37bc8c514e9e852cc0f87816dfb5da56cc34cb10
root <egoranatolich@gmai.com>
2022-01-06 02:39:26 +0300

    Store

--- store/wsgi — копия.py
+++ store/wsgi — копия.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.ENVIRON [ "DJANGO_SETTINGS_MODULE" ]  =  "myproject.settings"
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'store.settings')
 
 application = get_wsgi_application()
