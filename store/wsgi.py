5ba6fc997329024bd0b28e4507828d55df18acf2
root <egoranatolich@gmai.com>
2022-01-06 10:05:19 +0300

    Store

--- store/wsgi.py
+++ store/wsgi.py
@@ -8,14 +8,9 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-import sys
-
-if path not in sys.path:
-    sys.path.append(path)
-
-os.ENVIRON[ "DJANGO_SETTINGS_MODULE" ]  =  'store.settings'
 
 from django.core.wsgi import get_wsgi_application
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'store.settings')
 
 application = get_wsgi_application()
